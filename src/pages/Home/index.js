d12efe5186a18b61b4cb2d188bf7d5c6a4a9a933
Giudicelli <giudicellielias@gmail.com>
2023-08-11 10:56:21 -0300

    Estrura iniciial do projeto

---
+++ src/pages/Home/index.js
@@ -0,0 +1,11 @@
+import styles from "./Home.module.scss";
+
+const Home = () =>{
+    return(
+        <div className={styles.home}>
+            sssss
+        </div>
+    )
+}
+
+export default Home;
