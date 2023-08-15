4691d2db752a5e267c1c13257358847c7722ee93
Giudicelli Elias <86128363+giudicellisilva@users.noreply.github.com>
2023-08-15 19:03:31 -0300

    Componente de miniatura

---
+++ src/components/Miniature/index.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import styles from "./Miniature.module.scss";
+import arrow from "assets/icons/arrow-up.svg";
+import arrowRight from "assets/icons/arrow-up-right.svg";
+
+import "./animacao.css"
+
+const Miniature = ({miniature, description}) =>{
+
+    const [visible, setVisible] = useState(false);
+
+    return(
+        <div className={`${styles.miniature} ${visible? "active" : ""}`} onMouseDown={() => setVisible(false)} >
+            { visible ? <div className={`${styles.arrow} ${visible? "activeArrow" : ""}`} onClick={() => setVisible(false)}><img src={arrowRight} /></div> : <div className={styles.arrow} onClick={() => setVisible(true)}><img src={arrow} /></div>}
+            <img className={styles.miniature__img} src={miniature} alt={description}  />  
+        </div>
+    )
+}
+
+export default Miniature;
--- src/pages/OsteologiaEquina/EsqueletoAxial/Cranio/index.jsx
+++ src/pages/OsteologiaEquina/EsqueletoAxial/Cranio/index.jsx
@@ -2,6 +2,8 @@ import ModelViewer from "components/ModelViewer";
 import styles from "./Cranio.module.scss";
 import cranio from "assets/modelos3d/Cranio.glb"
 import Back from "components/Back";
+import Miniature from "components/Miniature";
+import cranioMiniature from "assets/miniaturas/Crânio.png";
 
 const Cranio = () =>{
     return(
@@ -11,6 +13,7 @@ const Cranio = () =>{
                 <h2>Cranio Equino</h2>
             </div>
             <ModelViewer modelo={cranio} ></ModelViewer>
+            <Miniature miniature={cranioMiniature} description={"indicação do crânio no esqueleto completo"} />
         </div>
     )
 }
